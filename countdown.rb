2487c874bab80618082dd7cfda96fe4bc943caa3
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-27 07:44:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,16 +5,17 @@ def countdown (number)
     puts "#{number} SECOND(S)!" 
      number -=1
   end
-  puts "HAPPY NEW YEAR!"
+ "HAPPY NEW YEAR!"
 end
 
 
 
 def countdown_with_sleep (number)
  number = 5
- while number >= 5
+ while number > 0
     puts "#{number} SECOND(S)!" 
      number -=1
+     sleep (1)
   end
   
 end
